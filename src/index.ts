7cc8a123bd55f813df3013882689365e71d8cbe5
facundotdf <104038338+facundotdf@users.noreply.github.com>
2022-05-03 19:07:35 -0300

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,7 @@
+let tabla = prompt("ingrese la tabla");
+let hasta = prompt("hasta");
+for (let i = 1; i < hasta; i++) {
+  document.write(tabla + " x " + i + " = " + tabla * i + "<br>");
+}
+
+console.log(tabla);
